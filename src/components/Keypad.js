0d937fd3b2edc7bcb1fdaae3a4c78432c6e3c92b
Billy <billychorey@gmail.com>
2023-09-23 13:03:35 -0400

    react hooks event handling lab

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,15 @@
-// Code Keypad Component Here
-
+import React from 'react'
 function Keypad (){
+   
+    function handleInput() {
+        console.log('Entering password...');
+    }
+
+
     return (
-        <div></div>
+        <div>
+            <input type="password" id="input" onChange={handleInput}/>
+        </div>
     )
 }
 
